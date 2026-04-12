d30ee0e5905bb99642c85f836437e5bb66dd561b
FG <65227683+groenerfelix@users.noreply.github.com>
2026-04-12 18:14:39 +0200

    first complete version of react rework

---
+++ src/data/cv-download.ts
@@ -0,0 +1,8 @@
+import type { SocialLink } from "@/types/content"
+
+export const cvDownloadLink: SocialLink = {
+  label: "CV PDF",
+  href: "/cv_felix_groener_2026_04.pdf",
+  download: "felix-groener-cv.pdf",
+  iconSrc: "/logos/download-icon.svg",
+}
